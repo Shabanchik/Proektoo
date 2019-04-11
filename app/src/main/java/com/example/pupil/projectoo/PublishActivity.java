df7d820ade204167977795c782b1e946496a1e93
Shabanchik <anastasiyash2003@gmail.com>
2019-04-11 17:56:55 +0300

    hrtrsthjtryjr

--- app/src/main/java/com/example/pupil/projectoo/PublishActivity.java
+++ app/src/main/java/com/example/pupil/projectoo/PublishActivity.java
@@ -4,6 +4,8 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -12,28 +14,39 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import org.w3c.dom.Comment;
+
 import java.util.HashMap;
 import java.util.Map;
 
-public class PublicActivity extends AppCompatActivity {
+public class PublishActivity extends AppCompatActivity {
     public EditText editName;
     public EditText editSurName;
     public EditText editText;
     public TextView name;
     public TextView surname;
     public TextView text;
+    private Button publish;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_public);
+        setContentView(R.layout.activity_publish);
         name=findViewById(R.id.name);
         surname=findViewById(R.id.surname);
         text=findViewById(R.id.text);
         editName=findViewById(R.id.editName);
         editSurName=findViewById(R.id.editSurName);
         editText=findViewById(R.id.editText);
+        publish=findViewById(R.id.publish);
+        publish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Specs car = createCar(editSurName.getText().toString(),editName.getText().toString(), editText.getText().toString());
+                start(car.getType(),car.getPassengers(),car.getName());
+            }
+        });
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         Map<String, Object> user = new HashMap<>();
@@ -42,8 +55,8 @@ public class PublicActivity extends AppCompatActivity {
         user.put("Text", "BlaBlaBlaBlaBla");
         user.put("Comment", "what was that???");
 
-
         db.collection("Blog")
+
                 .add(user)
                 .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                     @Override
@@ -57,6 +70,10 @@ public class PublicActivity extends AppCompatActivity {
                         Log.w("", "Error adding document", e);
                     }
                 });
+    }
+    public static Comment createCar(String name, String surname, String mtext){
+        Comment specs = new Comment(type, pas, maxSpeed);
+        return specs;
     }
 
 }
